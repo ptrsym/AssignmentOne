3f9360cf9bf12ef6a18e2b13a7b6cdd4108c999d
Peter Symoniw <ptr.sym@gmail.com>
2023-03-16 15:04:34 +1000

    Initial Commit

---
+++ AssignmentOne/AssignmentOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentOneApp.swift
+//  AssignmentOne
+//
+//  Created by peter on 16/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentOne/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AssignmentOne
+//
+//  Created by peter on 16/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
